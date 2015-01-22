afe6482d11419fc152ddf5012f15b5a56d233d94
Matt Parsons <parsonsmatt@gmail.com>
2015-01-22 18:18:11 -0500

    add some stuff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,6 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+alias gffs='git flow feature start'
+alias gfd='git flow feature finish'
