dc04df4a92dea4cfb3e395680a06548d9d0c2801
Matt Parsons <parsonsmatt@gmail.com>
2015-01-22 19:39:56 -0500

    remove vim-markdown

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,4 +19,4 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 
 alias gffs='git flow feature start'
-alias gfd='git flow feature finish'
+alias gffd='git flow feature finish'
