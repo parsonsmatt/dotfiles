e9c43d86daedaaa655de799ec5381240397daa79
Matt Parsons <parsonsmatt@gmail.com>
2015-04-08 20:26:53 -0400

    virtual box

--- misc/install.sh
+++ misc/install.sh
@@ -2,9 +2,13 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
+# Install VirtualBox PPA
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list.d/vbox.list
+
 sudo apt-get update
 
-sudo apt-get install zsh silversearcher-ag wallch google-chrome-stable -y
+sudo apt-get install zsh silversearcher-ag wallch google-chrome-stable virtualbox 4.3 dkms -y
 
 # install oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
