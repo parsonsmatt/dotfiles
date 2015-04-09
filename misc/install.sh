9c6383dbeadd0627190344aae242bf2d343a9b29
Matt Parsons <parsonsmatt@gmail.com>
2015-04-08 20:16:52 -0400

    do some install fixes

--- misc/install.sh
+++ misc/install.sh
@@ -1,1 +1,10 @@
-sudo apt-get install silversearcher-ag wallch
+# Install Google Chrome PPA
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+sudo apt-get update
+
+sudo apt-get install zsh silversearcher-ag wallch google-chrome-stable -y
+
+# install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
--- ruby/install.sh
+++ ruby/install.sh
@@ -1,8 +1,10 @@
-#! /bin/sh
+#! /bin/bash
+
+sudo apt-get install curl -y
 
 # Install rvm with bundler
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.0 --with-gems="bundler"
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.1 --with-gems="bundler"
 source ~/.rvm/scripts/rvm
 
 
