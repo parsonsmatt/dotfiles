1e5000b38141a646c67b0185d0a5c991071bcecc
Matt Parsons <parsonsmatt@gmail.com>
2015-04-09 19:45:53 -0400

    use cabal to update ghc-mod

--- haskell/install.sh
+++ haskell/install.sh
@@ -4,4 +4,7 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install cabal-install-1.20 ghc-7.8.3 happy-1.19.4 alex-3.1.3 ghc-mod -y
+sudo apt-get install cabal-install-1.20 ghc-7.8.3 happy-1.19.4 alex-3.1.3 -y
+
+cabal update
+cabal install ghc-mod
