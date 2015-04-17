510b0c13aa62c85e3a0ddb2b4892c1468db6e485
Matt Parsons <parsonsmatt@gmail.com>
2015-04-17 14:32:12 -0400

    fixed

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Note the new setup script name for Node.js v0.12
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+sudo apt-get update
 sudo apt-get install nodejs -y
 
