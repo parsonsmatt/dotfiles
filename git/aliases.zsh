1de9cd8ff064ea536d2c3bb23927d25e671cb3f6
Matt Parsons <parsonsmatt@gmail.com>
2015-05-28 22:53:01 -0400

    alias for git clone

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,5 @@
+# vi: set noundofile :
+
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -12,3 +14,7 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 
 alias gffs='git flow feature start'
 alias gffd='git flow feature finish'
+
+function gclo() {
+  git clone "git@github.com:$1"
+}
