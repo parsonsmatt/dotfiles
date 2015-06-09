0404cd1f456db1874b16187f6dab654baa8f4a1b
Matt Parsons <parsonsmatt@gmail.com>
2015-06-09 19:43:16 -0400

    oops

--- haskell/install.sh
+++ haskell/install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install cabal-install-1.22 ghc-7.10.2 happy-1.19.5 alex-3.1.5 -y
+sudo apt-get install cabal-install-1.22 ghc-7.10.2 happy-1.19.5 alex-3.1.4 -y
 
 cabal update
 cabal install haddock ghcid hoogle pointfree hdevtools hlint purescript
