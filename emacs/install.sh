bb5c6146ed4450847de1f8137d1b5c911f92562c
Matt Parsons <parsonsmatt@gmail.com>
2015-06-09 20:15:21 -0400

    yall

--- emacs/install.sh
+++ emacs/install.sh
@@ -2,4 +2,6 @@
 
 sudo apt-get install emacs -y
 
-git clone --recursive http://github.com/syl20bnr/spacemacs ~/.emacs.d
+if [ -d "~/.emacs.d" ]; then
+        git clone --recursive http://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
--- haskell/install.sh
+++ haskell/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
-sudo apt-get update
 sudo apt-get install cabal-install-1.22 ghc-7.10.2 happy-1.19.5 alex-3.1.4 -y
+
+reload!
 
 cabal update
 cabal install haddock ghcid hoogle pointfree hdevtools hlint purescript
