ccab178f9ded7d8e2d2d537fac82bbe405e3acba
Matt Parsons <parsonsmatt@gmail.com>
2015-06-17 21:01:40 -0400

    Add stack

--- haskell/install.sh
+++ haskell/install.sh
@@ -2,8 +2,12 @@
 
 sudo apt-get install software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
-sudo apt-get install cabal-install-1.22 ghc-7.10.2 happy-1.19.5 alex-3.1.4 -y
 
+# Stack :D
+wget -q -O- http://download.fpcomplete.com/ubuntu/fpco.key | sudo apt-key add -
+echo 'deb http://download.fpcomplete.com/ubuntu/vivid stable main' | sudo tee /etc/apt/sources.list.d/fpco.list
+sudo apt-get update
+sudo apt-get install stack cabal-install-1.22 ghc-7.10.2 happy-1.19.5 alex-3.1.4 -y
 reload!
 
 cabal update
