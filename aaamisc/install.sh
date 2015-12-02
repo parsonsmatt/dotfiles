4fbcdd8299b92d80c4dbdfdcaaca191dd5123a7c
Matt Parsons <parsonsmatt@gmail.com>
2015-12-02 16:52:59 -0500

    rm chrome

--- aaamisc/install.sh
+++ aaamisc/install.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Install Google Chrome PPA
-if [ ! hash google-chrome ]; then
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-fi
-
-# Install VirtualBox PPA
-if [ ! hash virtualbox ]; then
-    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-    sudo echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list.d/vbox.list
-fi
-
 sudo apt-get update
 
-sudo apt-get install nfs-common redshift zsh silversearcher-ag wallch google-chrome-stable virtualbox-4.3 dkms -y
+sudo apt-get install nfs-common redshift zsh silversearcher-ag wallch dkms -y
 
 # install oh-my-zsh
 if [ ! -f ~/.oh-my-zsh ]; then
