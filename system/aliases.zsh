572080cb90df0040db6a8ec56763efb46266106e
Matt Parsons <parsonsmatt@gmail.com>
2016-01-21 12:34:35 -0500

    Add alias for 4760 vm

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,3 +1,5 @@
 alias nike='ssh parsons@nike.cs.uga.edu'
 # vi: set noundofile :
 alias l3='sudo LD_LIBRARY_PATH=/usr/local/lib openconnect --juniper --no-cert-check ssl.layer3com.com'
+
+alias vm4760='ssh mbp1988@172.17.149.120'
