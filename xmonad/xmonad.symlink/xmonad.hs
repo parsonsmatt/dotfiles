4dd3df04196d95246dacce4a5500e5395a9a9534
Matt Parsons <parsonsmatt@gmail.com>
2016-04-19 19:51:07 -0400

    more xmonad

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,12 +1,29 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import Graphics.X11.ExtraTypes.XF86
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Actions.Volume
+
 import Control.Monad
+import Data.Ratio ((%))
+import Graphics.X11.ExtraTypes.XF86
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 import System.IO
+import XMonad
+import XMonad
+import XMonad.Actions.Plane
+import XMonad.Actions.Volume
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.Circle
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.Grid
+import XMonad.Layout.IM
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
 
 main :: IO ()
 main = do
@@ -15,14 +32,17 @@ main = do
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , workspaces = myWorkspaces
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , modMask = myModMask     -- Rebind Mod to the Windows key
         , focusedBorderColor = "purple"
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+        } `additionalKeys` myKeys
+
+myKeyBindings = 
+        [ ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         , ((0, xF86XK_AudioLowerVolume), void $ lowerVolume 1)
@@ -31,3 +51,56 @@ main = do
         , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
         , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
         ]
+
+myModMask = mod4Mask
+
+myWorkspaces =
+  [
+    "7:Chat",  "8:Dbg", "9:Pix",
+    "4:Docs",  "5:Dev", "6:Web",
+    "1:Term",  "2:Hub", "3:Mail",
+    "0:VM",    "Extr1", "Extr2"
+  ]
+
+startup = "5:Dev"
+
+numPadKeys =
+  [
+    xK_KP_Home, xK_KP_Up, xK_KP_Page_Up
+    , xK_KP_Left, xK_KP_Begin,xK_KP_Right
+    , xK_KP_End, xK_KP_Down, xK_KP_Page_Down
+    , xK_KP_Insert, xK_KP_Delete, xK_KP_Enter
+  ]
+
+numKeys =
+  [
+    xK_7, xK_8, xK_9
+    , xK_4, xK_5, xK_6
+    , xK_1, xK_2, xK_3
+    , xK_0, xK_minus, xK_equal
+  ]
+
+-- Here, some magic occurs that I once grokked but has since
+-- fallen out of my head. Essentially what is happening is
+-- that we are telling xmonad how to navigate workspaces,
+-- how to send windows to different workspaces,
+-- and what keys to use to change which monitor is focused.
+myKeys = myKeyBindings ++
+  [
+    ((m .|. myModMask, k), windows $ f i)
+       | (i, k) <- zip myWorkspaces numPadKeys
+       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  ] ++
+  [
+    ((m .|. myModMask, k), windows $ f i)
+       | (i, k) <- zip myWorkspaces numKeys
+       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  ] ++
+  M.toList (planeKeys myModMask (Lines 4) Finite) ++
+  [
+    ((m .|. myModMask, key), screenWorkspace sc
+      >>= flip whenJust (windows . f))
+      | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
+      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+  ]
+
