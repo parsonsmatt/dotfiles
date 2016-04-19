54eae5a3bda9984464b00b1ac88fd4a353bc2988
Matt Parsons <parsonsmatt@gmail.com>
2016-04-18 23:20:46 -0400

    stuff

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"
 
@@ -20,4 +21,7 @@ main = do
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
+        , ((0, xK_F11), spawn "amixer set Master 4-")
+        , ((0, xK_F12), spawn "amixer set Master 4+")
+        , ((0, xK_F10), spawn "amixer set Master toggle")
         ]
