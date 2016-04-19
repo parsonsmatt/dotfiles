bde69bb943a4ae24644cc2f9b111438cf780acf6
Matt Parsons <parsonsmatt@gmail.com>
2016-04-18 22:57:54 -0400

    Xmonad initial commit

--- xmonad/install.sh
+++ xmonad/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 # Script to install xmonad and all dependencies.
+
+sudo apt-get install -y xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev xmobar suckless-tools xscreensaver stalonetray
