ce50991561f7f859e1a76d06b538f8f44a9becf8
Matt Parsons <parsonsmatt@gmail.com>
2016-04-19 20:31:01 -0400

    add redshift

--- xmonad/install.sh
+++ xmonad/install.sh
@@ -2,7 +2,7 @@
 
 # Script to install xmonad and all dependencies.
 
-sudo apt-get install -y xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev suckless-tools xscreensaver stalonetray libiw-dev libasound2-dev c2hs libxpm-dev xbacklight
+sudo apt-get install -y xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev suckless-tools xscreensaver stalonetray libiw-dev libasound2-dev c2hs libxpm-dev xbacklight feh
 
 cabal install xmonad-extras
 cabal install alsa-core -f-pkgConfig
