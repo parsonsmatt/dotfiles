5a2e45a402ef192035811e989f24b8cdc2c2b66a
Matt Parsons <parsonsmatt@gmail.com>
2016-05-01 16:16:12 -0400

    autolock

--- xmonad/install.sh
+++ xmonad/install.sh
@@ -2,7 +2,7 @@
 
 # Script to install xmonad and all dependencies.
 
-sudo apt-get install -y xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev suckless-tools xscreensaver stalonetray libiw-dev libasound2-dev c2hs libxpm-dev xbacklight feh libxml2-dev xcompmgr moreutils
+sudo apt-get install -y xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev suckless-tools xscreensaver stalonetray libiw-dev libasound2-dev c2hs libxpm-dev xbacklight feh libxml2-dev xcompmgr moreutils xautolock
 
 cabal install xmonad-extras
 cabal install alsa-core -f-pkgConfig
