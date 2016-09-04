48455a0ca7518bf0d639992427b50795431648c8
5outh <bkovach13@gmail.com>
2016-09-04 18:08:51 -0400

    Fish

---
+++ git/functions.fish
@@ -0,0 +1,22 @@
+# vi: set noundofile :
+
+# The rest of my fun git aliases
+alias gl 'git pull --prune'
+alias glog "git log --graph --pretty format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date relative"
+alias gp 'git push origin HEAD'
+alias gd 'git diff'
+alias gc 'git commit'
+alias gca 'git commit -a'
+alias gco 'git checkout'
+alias gcb 'git copy-branch-name'
+alias gb 'git branch'
+alias gs 'git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+alias gffs 'git flow feature start'
+alias gffd 'git flow feature finish'
+
+function gclo 
+  git clone "git@github.com:$1"
+end
+
+# vi: set noundofile :
