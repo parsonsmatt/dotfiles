dd3d4da5aa6250ab00226788a090bd2d727e7304
5outh <bkovach13@gmail.com>
2017-07-28 14:51:02 -0400

    Add back git functions

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,1 @@
-alias nike='ssh parsons@nike.cs.uga.edu'
-# vi: set noundofile :
-alias l3='sudo LD_LIBRARY_PATH=/usr/local/lib openconnect --juniper --no-cert-check ssl.layer3com.com'
 
-alias vm4760='ssh mbp1988@172.17.149.120'
