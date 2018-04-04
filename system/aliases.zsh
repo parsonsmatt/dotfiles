47146a13de7dff259e82498b6dd0b0348245df9b
Matt Parsons <parsonsmatt@gmail.com>
2018-04-04 13:44:32 -0600

    add some hlps

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,7 @@
-alias nike='ssh parsons@nike.cs.uga.edu'
 # vi: set noundofile :
 alias l3='sudo LD_LIBRARY_PATH=/usr/local/lib openconnect --juniper --no-cert-check ssl.layer3com.com'
 
 alias vm4760='ssh mbp1988@172.17.149.120'
+
+alias edit-install='nvim ~/.dotfiles/script/install'
+alias run-install='~/.dotfiles/script/install'
